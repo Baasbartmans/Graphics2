781e970f9823cb1f11d3df3cdea7336aeec9a204
LAPTOP-9PR1HF00\bkolb <b.kolb@live.nl>
2017-05-11 16:38:55 +0200

    Ray2

--- Raytracer.cs
+++ Raytracer.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Template;
+using template;
 
-namespace template
+namespace Template
 {
     class Raytracer
     {
