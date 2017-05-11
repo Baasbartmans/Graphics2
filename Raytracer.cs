3a3be19046659a12a8d06d63fa28d3096592a598
LAPTOP-9PR1HF00\bkolb <b.kolb@live.nl>
2017-05-11 16:36:44 +0200

    Raytracer setup

--- Raytracer.cs
+++ Raytracer.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Template;
 
 namespace template
 {
     class Raytracer
     {
         public Raytracer()
+        {
+
+        }
+
+        public void Render(Camera cam, Scene scene, Surface displaySurf )
         {
 
         }
